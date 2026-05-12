fbb167d67cdadb14607fe1327c8d27c65c0b6ad5
Bartek <kierunb@gmail.com>
2026-05-12 19:51:26 +0200

    Add project files.

---
+++ WebApiWorkshop.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿namespace WebApiWorkshop.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+
+    public void Test2()
+    {
+        Assert.True(true);
+    }
+
+    public void Test3()
+    {
+        Assert.True(true);
+    }
+
+    public void Test4()
+    {
+        Assert.True(true);
+    }
+
+    public void Test5()
+    {
+        Assert.True(true);
+    }
+
+    public void Test6()
+    {
+        Assert.True(true);
+    }
+
+    public void Test7()
+    {
+        Assert.True(true);
+    }
+
+    public void Test8()
+    {
+        Assert.True(true);
+    }
+}
